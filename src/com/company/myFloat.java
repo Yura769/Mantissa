83bbd7bc49b911218974522c5767011ed38f83e4
Yura <yuralong.skiy@mail.ru>
2020-11-04 21:42:18 +0300

    homework1

---
+++ src/com/company/myFloat.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class myFloat
+{
+    private long mant;
+    private long exp;
+    public myFloat(long mant, long exp)
+    {
+        this.exp = exp;
+        this.mant = mant;
+    }
+    public void diff(myFloat b)
+    {
+        long mant1 = b.mant;
+        long 
+    }
+}
